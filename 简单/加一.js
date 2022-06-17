6b5009d099d754ea44ecd31de059688a455e5d2c
zk <1099571219@qq.com>
2022-06-17 09:51:49 +0800

    加一

--- 简单/加一.js
+++ 简单/加一.js
@@ -1,7 +1,17 @@
-const digits = [4,3,2,1]
-var plusOne = function(digits) {
-    if(digits.length===1) return digits[0] +=1;
-    digits[digits.length-1] +=1;
-    return digits;
+const digits = [0]
+var plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (i === 0) {
+            digits[0]===9 ? (digits[0]=0,digits.unshift(1)) : digits[0]++;
+            break;
+        }
+        if(digits[i]===9){
+            digits[i]=0;
+        }else{
+            digits[i]++;
+            break;
+        }
+    }
+    return digits
 };
-console.log(plusOne([1,2,3]));
+console.log(plusOne(digits));
